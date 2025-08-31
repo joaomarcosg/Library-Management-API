f5cb074c483e94c7eb79c628528a3a0a9849f9fa
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-31 18:38:12 -0300

    FEAT: adds body request handle

--- internal/controller/signupUser.go
+++ internal/controller/signupUser.go
@@ -1,7 +1,22 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joaomarcosg/Library-Management-API/internal/configuration/resterror"
+	"github.com/joaomarcosg/Library-Management-API/internal/controller/model/request"
 )
 
-func SignupUser(c *gin.Context) {}
+func SignupUser(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := resterror.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error = %s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+}
