ca774c797e01fc4ef77f269b4208544024320156
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-08-31 17:38:55 -0300

    FEAT: adds route handlers

--- internal/controller/routes/routes.go
+++ internal/controller/routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joaomarcosg/Library-Management-API/internal/controller"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUserById/:userId")
-	r.GET("/getUserByEmail/:userEmail")
-	r.POST("/signupUser/")
-	r.PUT("/updateUser/:userId")
-	r.DELETE("/deleteUser/:userId")
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/signupUser/", controller.SignupUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
